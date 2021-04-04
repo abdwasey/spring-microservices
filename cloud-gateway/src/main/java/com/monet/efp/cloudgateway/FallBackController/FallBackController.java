7bb4355c9f00db685ea732e0ad9c5a2fefc17246
Wasey <abdul.wasey17@outlook.com>
2021-04-04 21:15:59 +0500

    Wasey Commit

---
+++ cloud-gateway/src/main/java/com/monet/efp/cloudgateway/FallBackController/FallBackController.java
@@ -0,0 +1,21 @@
+package com.monet.efp.cloudgateway.FallBackController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/*@RestController
+@RequestMapping(value = "/")*/
+public class FallBackController {
+
+
+  /*  @RequestMapping(value = "/userFallBack", method = RequestMethod.GET)
+    public String userServiceFallBackMetod(){
+        return "User Service Tak time to Response";
+    }
+
+    @RequestMapping(value = "/roleFallBack", method = RequestMethod.GET)
+    public String roleServiceFallBackMetod(){
+        return "Role Service Tak time to Response";
+    }*/
+}
